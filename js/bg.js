984c7edd501e9a5fa479c86cc6a6abe19cd52a48
Al145 <47367383+Al145@users.noreply.github.com>
2019-12-11 21:23:32 +0200

    some fix

--- js/bg.js
+++ js/bg.js
@@ -1,14 +1,13 @@
-window.setInterval(function () {
-    let headerImg =
-            [
-                'img/back1.jpg',
-                'img/back2.jpg',
-                'img/back3.jpg',
-                'img/back4.jpg',
-                'img/back5.jpg',
-                'img/back6.jpg',
-                'img/back7.jpg'
-            ],
+window.setInterval(function() {
+    let headerImg = [
+            'img/back1.jpg',
+            'img/back2.jpg',
+            'img/back3.jpg',
+            'img/back4.jpg',
+            'img/back5.jpg',
+            'img/back6.jpg',
+            'img/back7.jpg'
+        ],
         headerImgName = Math.floor(Math.random() * headerImg.length),
         headerBg = document.getElementById('headerBg');
 
